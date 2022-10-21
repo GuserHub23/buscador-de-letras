d1439388a14cedd504dbe9b8b7da90d96d122b04
Juan <pablogomez@gmail.com>
2022-10-21 15:15:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AppLetras from './components/AppLetras'
+import { LetrasProvider } from './context/LetrasProvider'
+
+const App = () => {
+    return (
+        <LetrasProvider>
+            <AppLetras/>
+        </LetrasProvider>
+    )
+}
+
+export default App
---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,33 @@
+import Alerta from './Alerta'
+import Formulario from './Formulario'
+
+import useLetras from '../hooks/useLetras'
+import Letra from './Letra'
+import Spinner from './Spinner'
+
+const AppLetras = () => {
+    
+    const { alerta, letra, cargando } = useLetras()
+
+    return (
+        <>
+            <header>Búsqueda de Letras de Canciones</header>
+            
+
+            <Formulario/>
+            
+            <main>
+                {
+                    alerta 
+                        ? <Alerta>{alerta}</Alerta> 
+                        : letra 
+                            ? <Letra/> 
+                            : cargando ? <Spinner/>
+                                : <p className='text-center'>Busca letras de tus artistas favoritos</p>
+                }
+            </main>
+        </>
+    )
+}
+
+export default AppLetras
